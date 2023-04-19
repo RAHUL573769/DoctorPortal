dd616fa93d92b354ef49fec52c4b623bb9deb497
Rahul Rudra <rahulrudra665@gmail.com>
2023-04-19 16:09:36 +0600

    modal commit

---
+++ FrontEnd/reactproject/src/Componenets/AppointmentRouter/AppointOption.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const AppointOption = ({option,setTreatment}) => {
+ console.log(option)
+const{id,name,slots}=option;
+  return (
+    <div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body text-center">
+          <h2 className="card-title">{name}</h2>
+          <p>{slots.length > 0 ? slots[0] : "Try Another Day"}</p>
+          <p>
+            {slots.length}
+            {slots.length > 1 ? "spaces" : "space"} Available
+          </p>
+          <div className="card-actions justify-end">
+          
+            <label
+              onClick={() =>setTreatment(option)}
+              htmlFor="my-modal-3"
+              className="btn  btn-primary"
+            >
+              Book Appointment
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppointOption;
--- FrontEnd/reactproject/src/Componenets/AppointmentRouter/AvailableAppointments.js
+++ FrontEnd/reactproject/src/Componenets/AppointmentRouter/AvailableAppointments.js
@@ -1,13 +1,37 @@
-import { format } from 'date-fns';
-import React from 'react';
+import { format } from "date-fns";
+import React, { useEffect, useState } from "react";
+import AppointOption from "./AppointOption";
+import BookingModal from "./Modal/BookingModal";
 
 const AvailableAppointments = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <p className='text-center'>Available Appointmnets {format(props.selected, 'PP')}</p>
-        </div>
-    );
+  const [appointmentOptions, setAppointment] = useState([]);
+
+  const [treatment,setTreatment]=useState(null);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setAppointment(data));
+  }, []);
+  return (
+    <div>
+      <p className="text-center">
+        Available Appointmnets {format(props.selected, "PP")}
+      </p>
+
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+{/* {
+    appointmentOptions.map(options=><AppointOption key={options._id} options={options}></AppointOption>)
+} */}
+{
+    appointmentOptions.map(option=><AppointOption key={option._id} setTreatment={setTreatment} option={option}></AppointOption>)
+}
+
+<BookingModal treatment={treatment} ></BookingModal>
+
+      </div>
+    </div>
+  );
 };
 
 export default AvailableAppointments;
